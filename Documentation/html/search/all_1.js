c0e21d7ae6fb39284397c9a1c6d912370c090e77
Frederick Hastedt <ben@hastedts.de>
2018-06-13 05:54:21 +0200

    Update the documentation with brief class descriptions.

--- Documentation/html/search/all_1.js
+++ Documentation/html/search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['compare',['Compare',['../class_f_bson_object.html#a33c96802fcc2f7267a2d9b4c315b751c',1,'FBsonObject']]],
-  ['compareequal',['CompareEqual',['../class_f_bson_value.html#a979a25eef93a5f0398ecc372518a5c43',1,'FBsonValue']]]
+  ['compareequal',['CompareEqual',['../class_f_bson_value.html#a979a25eef93a5f0398ecc372518a5c43',1,'FBsonValue']]],
+  ['copy',['Copy',['../class_f_bson_object.html#aa6a8380b281cf120fe859fa27d470d18',1,'FBsonObject']]]
 ];
